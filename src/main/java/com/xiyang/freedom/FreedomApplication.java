a30faf9dc2500138a9654903e4853a395a72602d
xiyang1024 <xiyang.ycj@qq.com>
2019-05-16 18:02:48 +0800

    提交了

---
+++ src/main/java/com/xiyang/freedom/FreedomApplication.java
@@ -0,0 +1,13 @@
+package com.xiyang.freedom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreedomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreedomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiyang/freedom/FreedomApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xiyang.freedom;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FreedomApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
